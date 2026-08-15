226cae061b7bcc01440b47284ef502444cc9ed91
Raghavendra Singh <raghavendrashivam474@gmail.com>
2026-08-16 04:48:09 +0530

    feat: compose Aryntra landing page from section components

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,23 @@
+﻿import { Navbar } from "@/components/layout/Navbar";
+import { Footer } from "@/components/layout/Footer";
+import { Hero } from "@/components/sections/Hero";
+import { Introduction } from "@/components/sections/Introduction";
+import { DecisionLayer } from "@/components/sections/DecisionLayer";
+import { Exploration } from "@/components/sections/Exploration";
+import { Future } from "@/components/sections/Future";
+
+export default function HomePage() {
+  return (
+    <>
+      <Navbar />
+      <main id="main-content" tabIndex={-1}>
+        <Hero />
+        <Introduction />
+        <DecisionLayer />
+        <Exploration />
+        <Future />
+      </main>
+      <Footer />
+    </>
+  );
+}
